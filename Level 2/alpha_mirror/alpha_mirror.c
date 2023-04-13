1a267422115373e04dc00007cf12df9c37b223d8
fsantama <fransantamaria95@gmail.com>
2023-04-13 13:19:45 +0200

    alpha_mirror added

---
+++ Level 2/alpha_mirror/alpha_mirror.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   alpha_mirror.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsantama <fsantama@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 13:03:52 by fsantama          #+#    #+#             */
+/*   Updated: 2023/04/13 13:18:15 by fsantama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_alpha_mirror(char *str)
+{
+	int	i;
+	char	c;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		c = str[i];
+		if (c >= 'a' && c <= 'z')
+			c = 'z' - (c - 'a');
+		else if (c >= 'A' && c <= 'Z')
+			c = 'Z' - (c - 'A');
+		write(1, &c, 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		ft_alpha_mirror(argv[1]);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
