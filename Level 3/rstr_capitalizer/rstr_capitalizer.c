05899b72ea146568192c65a0dba7f46daa4f4213
fsantama <fransantamaria95@gmail.com>
2023-05-05 16:15:21 +0200

    rstr_capitalizer.c

---
+++ Level 3/rstr_capitalizer/rstr_capitalizer.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsantama <fsantama@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 11:25:57 by fsantama          #+#    #+#             */
+/*   Updated: 2023/05/05 16:13:58 by fsantama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	rstr_capitalizer(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] - 'A' + 'a';
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (str[i + 1] == ' ' || str[i + 1] == '	' || str[i + 1] == 0)
+				str[i] = str[i] - 'a' + 'A';
+		}
+		write (1, &str[i], 1);
+		i++;
+	}
+	write (1, "\n", 1);
+}
+// La i se inicializa en 1 para saltar el nombre del programa
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (argv[i])
+		{
+			rstr_capitalizer(argv[i]);
+			i++;
+		}
+	}
+	else
+		write (1, "\n", 1);
+	return (0);
+}
