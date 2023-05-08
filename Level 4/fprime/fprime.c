ee6d81f5162f4c70669df6aa9629c28e81b26209
fsantama <fransantamaria95@gmail.com>
2023-05-08 11:30:06 +0200

    fprime added

---
+++ Level 4/fprime/fprime.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fprime.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsantama <fsantama@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 11:03:20 by fsantama          #+#    #+#             */
+/*   Updated: 2023/05/08 11:29:06 by fsantama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+// Buscamos los nº primos. Si %div == 0, dividimos n por div y seguimos buscando
+void	fprime(int n)
+{
+	int	div;
+
+	div = 2;
+	if (n == 1)
+		printf("1");
+	while (n >= div)
+	{
+		if (n % div == 0)
+		{
+			printf("%d", div);
+			if (n == div)
+				break ;
+			printf("*");
+			n /= div;
+			div = 1;
+		}
+		div++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		fprime(atoi(argv[1]));
+	printf("\n");
+	return (0);
+}
