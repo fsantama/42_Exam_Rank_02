271e600306f9dfe3253514951c421ae94ce3c92c
fsantama <fransantamaria95@gmail.com>
2023-05-10 13:18:42 +0200

    pass norminette and moulinette

--- Level 2/alpha_mirror/alpha_mirror.c
+++ Level 2/alpha_mirror/alpha_mirror.c
@@ -6,7 +6,7 @@
 /*   By: fsantama <fsantama@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 13:03:52 by fsantama          #+#    #+#             */
-/*   Updated: 2023/05/10 13:15:54 by fsantama         ###   ########.fr       */
+/*   Updated: 2023/05/10 13:18:27 by fsantama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ void	ft_alpha_mirror(char *str)
 	{
 		c = str[i];
 		if (c >= 'a' && c <= 'z')
-			c = 'z' - (c - 'a'); // Calculate the character by subtracting the distance
+			c = 'z' - (c - 'a');
 		else if (c >= 'A' && c <= 'Z')
-			c = 'Z' - (c - 'A'); // Calculate the character by subtracting the distance
+			c = 'Z' - (c - 'A');
 		ft_putchar(c);
 		i++;
 	}
