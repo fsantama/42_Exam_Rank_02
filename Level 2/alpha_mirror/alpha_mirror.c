420f34b40d7be7e11158c54fb712974786eefcec
fsantama <fransantamaria95@gmail.com>
2023-05-10 13:16:49 +0200

    subject and english commmets added

--- Level 2/alpha_mirror/alpha_mirror.c
+++ Level 2/alpha_mirror/alpha_mirror.c
@@ -6,7 +6,7 @@
 /*   By: fsantama <fsantama@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 13:03:52 by fsantama          #+#    #+#             */
-/*   Updated: 2023/05/10 13:12:34 by fsantama         ###   ########.fr       */
+/*   Updated: 2023/05/10 13:15:54 by fsantama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	ft_putchar(char c)
 	write (1, &c, 1);
 }
 
-// Función para reflejar alfabéticamente una cadena de caracteres
 void	ft_alpha_mirror(char *str)
 {
 	int		i;
@@ -28,9 +27,9 @@ void	ft_alpha_mirror(char *str)
 	{
 		c = str[i];
 		if (c >= 'a' && c <= 'z')
-			c = 'z' - (c - 'a'); // Calcula el carácter restando la distancia
+			c = 'z' - (c - 'a'); // Calculate the character by subtracting the distance
 		else if (c >= 'A' && c <= 'Z')
-			c = 'Z' - (c - 'A'); // Calcula el carácter restando la distancia
+			c = 'Z' - (c - 'A'); // Calculate the character by subtracting the distance
 		ft_putchar(c);
 		i++;
 	}
