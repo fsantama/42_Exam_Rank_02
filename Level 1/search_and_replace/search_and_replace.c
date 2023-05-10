1db9cd3de70b69c75579ce80c29340db0dbf21e2
fsantama <fransantamaria95@gmail.com>
2023-05-10 12:14:06 +0200

    search_and_replace modified
    Now exits a function named search_and_replace inside programm

--- Level 1/search_and_replace/search_and_replace.c
+++ Level 1/search_and_replace/search_and_replace.c
@@ -6,43 +6,48 @@
 /*   By: fsantama <fsantama@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 21:53:29 by fsantama          #+#    #+#             */
-/*   Updated: 2023/04/13 12:10:32 by fsantama         ###   ########.fr       */
+/*   Updated: 2023/05/10 12:12:36 by fsantama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	search_and_replace(char *str, char *s1, char *s2)
 {
 	int	i;
 
 	i = 0;
-	// Verificar si se pasaron exactamente 4 argumentos
-	if (argc == 4)
+	// Verificar si los caracteres de búsqueda y reemplazo son válidos
+	if ((((s1[i] >= 'a' && s1[i] <= 'z')
+				|| (s1[i] >= 'A' && s1[i] <= 'Z'))
+			&& (s1[i + 1] == 0)
+			&& ((s2[i] >= 'a' && s2[i] <= 'z')
+				|| (s2[i] >= 'A' && s2[i] <= 'Z'))
+			&& (s2[i + 1] == 0)))
 	{
-		// Verificar si los caracteres de búsqueda y reemplazo son válidos
-		if ((((argv[2][0] >= 'a' && argv[2][0] <= 'z')
-			|| (argv[2][0] >= 'A' && argv[2][0] <= 'Z'))
-			&& argv[2][1] == '\0')
-			&& ((argv[3][0] >= 'a' && argv[3][0] <= 'z')
-			|| (argv[3][0] >= 'A' && argv[3][0] <= 'Z'))
-			&& argv[3][1] == '\0')
+		// Recorrer la cadena de entrada
+		while (str[i])
 		{
-			// Recorrer la cadena de entrada usando un puntero
-			while (argv[1][i] != '\0')
-			{
-				// Realizar el reemplazo si el carácter es igual al de búsqueda
-				if (argv[1][i] == argv[2][0])
-					argv[1][i] = argv[3][0];
-				// Escribir el carácter actual en la salida estándar
-				write(1, &argv[1][i], 1);
+			// Realizar el reemplazo si el carácter es igual al de búsqueda
+			if (str[i] == s1[0])
+				str[i] = s2[0];
+			// Escribir el carácter actual en la salida estándar
+			ft_putchar(str[i]);
 				// Avanzar al siguiente carácter de la cadena
 				i++;
-			}
 		}
 	}
-	// Escribir una nueva línea al final de la salida
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 4)
+		search_and_replace(argv[1], argv[2], argv[3]);
 	write(1, "\n", 1);
-	// Devolver 0 para indicar que el programa finalizó correctamente
 	return (0);
 }
